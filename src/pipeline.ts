46af0bfd7c11b7ddd537875edfc014610c1dbf46
Ben Wainwright <ben.wainwright@cinch.co.uk>
2021-08-14 01:49:47 +0100

    Basically works

--- src/pipeline.ts
+++ src/pipeline.ts
@@ -1,24 +1,53 @@
-import { DetailKeys, EventOfKey, Event, Processor, EventProcessor, InputFunction } from "./types"
+import {
+  DetailKeys,
+  EventOfKey,
+  Event,
+  Processor,
+  EventPipelineInitialiser,
+} from "./types";
 
-const isKeyedEvent = <TEvent extends Event, K extends DetailKeys<TEvent>, E extends EventOfKey<TEvent, K>>(
+const isKeyedEvent = <
+  TEvent extends Event,
+  K extends DetailKeys<TEvent>,
+  E extends EventOfKey<TEvent, K>
+>(
   key: K,
-  event: TEvent,
-): event is E => event['detail-type'] === key
+  event: TEvent
+): event is E => event["detail-type"] === key;
 
-export const addProcessor = <TEvent extends Event, K extends DetailKeys<TEvent>>(key: K, processor: Processor<TEvent, K>) => {
-  return <S>(event: TEvent, state: S) => {
+export const addProcessor = <
+  TEvent extends Event,
+  K extends DetailKeys<TEvent>,
+  S
+>(
+  key: K,
+  processor: Processor<TEvent, K, S>
+) => {
+  return (event: TEvent, state: S) => {
     if (isKeyedEvent(key, event)) {
-      return { key, state: processor(key, event, state) }
+      return { key, state: processor(key, event, state) };
     }
-    return { state, key }
-  }
-}
+    return { state, key };
+  };
+};
 
-const isFunction = (thing: unknown): thing is Function => typeof thing === 'function'
-
-export const processEvent: EventProcessor =
-  (event, secondArg, ...args) => {
-    const funcs = isFunction(secondArg) ? [secondArg, ...args] : args
-    const state = isFunction(secondArg) ? undefined : secondArg
-    return funcs.reduce((accum, current) => (accum ? current(event, accum).state : undefined), state)
-}
+export const initPipeline: EventPipelineInitialiser = (
+  initialEvent,
+  initialState
+) => ({
+  step: <K extends DetailKeys<typeof initialEvent>>(
+    key: K,
+    processor: Processor<typeof initialEvent, K, typeof initialState>
+  ) => {
+    return (event: typeof initialEvent, state: typeof initialState) => {
+      if (isKeyedEvent(key, event)) {
+        return { key, state: processor(key, event, state) };
+      }
+      return { state, key };
+    };
+  },
+  execute: (...funcs) =>
+    funcs.reduce(
+      (currentState, chosenFunction) => chosenFunction(initialEvent, currentState).state, initialState
+    ),
+});
--- src/types.ts
+++ src/types.ts
@@ -1,35 +1,73 @@
-import { EventBridgeEvent } from 'aws-lambda'
+import { EventBridgeEvent } from "aws-lambda";
 
-export type Event = EventBridgeEvent<string, any>
+export type Event = EventBridgeEvent<string, any>;
 
-export type DetailKeys<T> = T extends EventBridgeEvent<infer K, unknown> ? K : never
+export type DetailKeys<T> = T extends EventBridgeEvent<infer K, any>
+  ? K
+  : never;
+export type Detail<T> = T extends EventBridgeEvent<string, infer D> ? D : never;
 
-export type EventOfKey<TEvent extends Event, TKey extends DetailKeys<TEvent>> = TEvent extends { 'detail-type': TKey }
-  ? TEvent
-  : never
+type EventsTypeMap<TEvent extends Event> = {
+  [K in TEvent["detail-type"]]: TEvent extends EventBridgeEvent<K, infer D>
+    ? D
+    : never;
+};
 
-export type NotFunction<T> = T extends Function ? never : T
+export type EventOfKey<
+  TEvent extends Event,
+  TKey extends TEvent["detail-type"]
+> = TEvent extends { "detail-type": TKey } ? TEvent : never;
 
-export interface Processor<TEvent extends Event, K extends DetailKeys<TEvent>> {
-  <E extends EventOfKey<TEvent, K>, S>(key: K, event: E, state: S):  S
-  <E extends EventOfKey<TEvent, K>>(key: K, event: E): void 
+export type RightEvent<
+  TEvent extends Event,
+  K extends string,
+  P extends (key: K, event: any) => any
+> = P extends (key: K, event: infer E) => any
+  ? E extends EventBridgeEvent<K, infer D>
+    ? D extends EventsTypeMap<TEvent>[K]
+      ? P
+      : never
+    : never
+  : never;
+
+export interface Processor<
+  TEvent extends Event,
+  K extends TEvent["detail-type"],
+  S
+> {
+  (key: K, event: EventBridgeEvent<K, EventsTypeMap<TEvent>[K]>, state: S): S;
+  (key: K, event: EventBridgeEvent<K, EventsTypeMap<TEvent>[K]>): void;
 }
 
-export type InputFunction<TEvent extends Event, K extends DetailKeys<TEvent>, S> = (
-  event: TEvent,
-  state: S,
-) => { key: K; state: S }
+export type InputFunction<
+  TEvent extends Event,
+  K extends DetailKeys<TEvent>,
+  S
+> = (event: TEvent, state: S) => { key: K; state: S };
 
-export type KeyedEventGuard = <TEvent extends Event, K extends DetailKeys<TEvent>, E extends EventOfKey<TEvent, K>>(key: K, event: TEvent) => event is E
+export type KeyedEventGuard = <
+  TEvent extends Event,
+  K extends DetailKeys<TEvent>,
+  E extends EventOfKey<TEvent, K>
+>(
+  key: K,
+  event: TEvent
+) => event is E;
 
-export interface EventProcessor {
-  <TEvent extends Event, K extends DetailKeys<TEvent>,  S>(
-    event: TEvent,
-    state: S,
-    ...funcs: (Exclude<DetailKeys<TEvent>, K> extends never ? InputFunction<TEvent, K, S> : never)[]): S | undefined
+type AddFunction<TEvent extends Event, S> = <K extends DetailKeys<TEvent>>(
+  key: K,
+  processor: Processor<TEvent, K, S | undefined>
+) => InputFunction<TEvent, K, S | undefined>;
 
-  <TEvent extends Event, K extends DetailKeys<TEvent>,  S>(
-    event: TEvent,
-    ...funcs: (Exclude<DetailKeys<TEvent>, K> extends never ? InputFunction<TEvent, K, S> : never)[]): S | undefined
+type ProcessorApi<TEvent extends Event, S> = {
+  step: AddFunction<TEvent, S>;
+  execute: <K extends DetailKeys<TEvent>>(
+    ...funcs: (Exclude<DetailKeys<TEvent>, K> extends never
+      ? InputFunction<TEvent, K, S | undefined>
+      : never)[]
+  ) => S | undefined;
+};
+
+export interface EventPipelineInitialiser {
+  <TEvent extends Event, S>(event: TEvent, state?: S): ProcessorApi<TEvent, S>;
 }
-
