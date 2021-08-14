7ad80b26a857460b75aaeeb459a467ce6f5c914e
Ben Wainwright <ben.wainwright@cinch.co.uk>
2021-08-14 02:50:26 +0100

    Added tests

--- src/pipeline.spec.ts
+++ src/pipeline.spec.ts
@@ -1,5 +1,4 @@
 import { EventBridgeEvent } from "aws-lambda";
-import { mock } from "jest-mock-extended"
 import { initPipeline } from "./pipeline"
 
 interface Thing {
@@ -37,13 +36,10 @@ describe("the event pipeline builder", () => {
       }
     }
 
-
     const handlerOne = (key: 'foo', event: EventBridgeEvent<'foo', Thing>) => fail('Handler one was not supposed to be called')
     const handlerTwo = (key: 'fo2o', event: EventBridgeEvent<'fo2o', Thing>) => fail('Handler two was not supposed to be called')
     const handlerThree = (key: 'anotherEvent', event: EventBridgeEvent<'anotherEvent', Thing2>) => expect(event).toBe(mockEvent)
     const handlerFour = (key: 'event', event: EventBridgeEvent<'event', Thing3>) => fail('Handler four was not supposed to be called')
-
-    const initialState = { foo: "bar" };
 
     const { execute, step } = initPipeline(mockEvent as IncomingEvent)
 
@@ -55,5 +51,156 @@ describe("the event pipeline builder", () => {
     )
 
     expect.assertions(1)
+  })
+
+  it("errors at build time if not all the events are handled", () => {
+    const mockEvent: IncomingEvent= {
+      id: '123',
+      version: 'a',
+      account: 'a',
+      time: 'a',
+      region: 'a',
+      resources: ['a'],
+      source: 'a',
+      'detail-type': 'anotherEvent',
+      detail: {
+        bar: 'foo'
+      }
+    }
+
+    const handlerOne = (key: 'foo', event: EventBridgeEvent<'foo', Thing>) => undefined
+    const handlerTwo = (key: 'fo2o', event: EventBridgeEvent<'fo2o', Thing>) => undefined
+    const handlerThree = (key: 'anotherEvent', event: EventBridgeEvent<'anotherEvent', Thing2>) => undefined
+
+    const { execute, step } = initPipeline(mockEvent as IncomingEvent)
+
+    execute(
+      // @ts-expect-error
+      step('foo', handlerOne),
+      step('fo2o', handlerTwo),
+      step('anotherEvent', handlerThree),
+    )
+  })
+
+  it("errors at build time if a key is used that doesn't exist", () => {
+    const mockEvent: IncomingEvent= {
+      id: '123',
+      version: 'a',
+      account: 'a',
+      time: 'a',
+      region: 'a',
+      resources: ['a'],
+      source: 'a',
+      'detail-type': 'anotherEvent',
+      detail: {
+        bar: 'foo'
+      }
+    }
+
+    const handlerOne = (key: 'foo', event: EventBridgeEvent<'foo', Thing>) => undefined
+    const handlerTwo = (key: 'fo2o', event: EventBridgeEvent<'fo2o', Thing>) => undefined
+    const handlerThree = (key: 'anotherEvent', event: EventBridgeEvent<'anotherEvent', Thing2>) => undefined 
+    const handlerFour = (key: 'event', event: EventBridgeEvent<'event', Thing3>) => undefined 
+
+    const { execute, step } = initPipeline(mockEvent as IncomingEvent)
+
+    execute(
+      // @ts-expect-error
+      step('foo0', handlerOne),
+      step('fo2o', handlerTwo),
+      step('anotherEvent', handlerThree),
+      step('event', handlerFour)
+    )
+  })
+
+  it("errors at build time if keys and handlers are mismatched", () => {
+    const mockEvent: IncomingEvent= {
+      id: '123',
+      version: 'a',
+      account: 'a',
+      time: 'a',
+      region: 'a',
+      resources: ['a'],
+      source: 'a',
+      'detail-type': 'anotherEvent',
+      detail: {
+        bar: 'foo'
+      }
+    }
+
+    const handlerOne = (key: 'foo', event: EventBridgeEvent<'foo', Thing>) => undefined
+    const handlerTwo = (key: 'fo2o', event: EventBridgeEvent<'fo2o', Thing>) => undefined
+    const handlerThree = (key: 'anotherEvent', event: EventBridgeEvent<'anotherEvent', Thing2>) => undefined 
+    const handlerFour = (key: 'event', event: EventBridgeEvent<'event', Thing3>) => undefined 
+
+    const { execute, step } = initPipeline(mockEvent as IncomingEvent)
+
+    execute(
+      // @ts-expect-error
+      step('anotherEvent', handlerOne),
+      step('fo2o', handlerTwo),
+      step('anotherEvent', handlerThree),
+      step('event', handlerFour)
+    )
+  })
+
+  it("errors at build time if the type is mismatched from the key in the handler event", () => {
+    const mockEvent: IncomingEvent= {
+      id: '123',
+      version: 'a',
+      account: 'a',
+      time: 'a',
+      region: 'a',
+      resources: ['a'],
+      source: 'a',
+      'detail-type': 'anotherEvent',
+      detail: {
+        bar: 'foo'
+      }
+    }
+
+    const handlerOne = (key: 'foo', event: EventBridgeEvent<'foo', Thing3>) => undefined
+    const handlerTwo = (key: 'fo2o', event: EventBridgeEvent<'fo2o', Thing>) => undefined
+    const handlerThree = (key: 'anotherEvent', event: EventBridgeEvent<'anotherEvent', Thing2>) => undefined 
+    const handlerFour = (key: 'event', event: EventBridgeEvent<'event', Thing3>) => undefined 
+
+    const { execute, step } = initPipeline(mockEvent as IncomingEvent)
+
+    execute(
+      // @ts-expect-error
+      step('foo', handlerOne),
+      step('fo2o', handlerTwo),
+      step('anotherEvent', handlerThree),
+      step('event', handlerFour)
+    )
+  })
+
+  it("compiles fine when multiple keys have the same handler", () => {
+    const mockEvent: IncomingEvent= {
+      id: '123',
+      version: 'a',
+      account: 'a',
+      time: 'a',
+      region: 'a',
+      resources: ['a'],
+      source: 'a',
+      'detail-type': 'anotherEvent',
+      detail: {
+        bar: 'foo'
+      }
+    }
+
+    const handlerOne = (key: 'foo' | 'fo2o', event: EventBridgeEvent<'foo' | 'fo2o', Thing>) => undefined
+    const handlerThree = (key: 'anotherEvent', event: EventBridgeEvent<'anotherEvent', Thing2>) => undefined 
+    const handlerFour = (key: 'event', event: EventBridgeEvent<'event', Thing3>) => undefined 
+
+    const { execute, step } = initPipeline(mockEvent as IncomingEvent)
+
+    execute(
+      step('foo', handlerOne),
+      step('fo2o', handlerOne),
+      step('anotherEvent', handlerThree),
+      step('event', handlerFour)
+    )
   })
 });
